fc91bf7c524b7461570d8c4aff465572a437e3aa
TheCodersYT <96958404+TheCodersYT@users.noreply.github.com>
2022-05-05 11:03:43 +0100

    Create main bot progra,

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,25 @@
+import nextcord as discord
+import os
+import sys
+import time
+import asyncio
 
+from nextcord.ext import commands
+from nextcord import Colour, Embed, SlashOption, slash_command
+
+intents = nextcord.Intents()
+
+bot = commands.Bot(command_prefix=">", intents=intents.all())
+
+token = "ODk3NTUzOTI5MTc5NjUyMTg2.GBI0Nv.amIkSRAWhpOi82ZWE3ler7_t6Nexuv9R37nj" # add ok on the end of the token
+
+@bot.event
+async def on_ready():
+  print(f"{bot.user.name}#{bot.user.discriminator} is ready!")
+  channel = bot.get_channel(967853173073129512)
+  
+  await channel.send("I am online.")
+  
+ 
+
+bot.run(token)
